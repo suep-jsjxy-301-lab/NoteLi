ed5b1c63bd8655535438ba52935a6423a1b1b6eb
kpop-lover-1 <18063028880@163.com>
2025-12-01 19:12:39 +0800

    feat(origins):添加了一些接口，允许跨域请求

--- packages/backend/app/main.py
+++ packages/backend/app/main.py
@@ -1,6 +1,6 @@
 # app/main.py
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.logger import logger
 from app.api.v1 import router
 from app.core.config import cfg
@@ -16,7 +16,18 @@ register_exception_handlers(app=app)
 # app.add_middleware(middleware_class=Always200Middleware)
 app.add_middleware(middleware_class=RequestTimerMiddleware)
 
+origins = [
+    "https://localhost",
+    "http://localhost",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router)
 logger.debug(f"Redis 配置: {cfg.redis.model_dump()}")
 
